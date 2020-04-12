8f0a55c0358edee09950c6defbdf620dab5113e5
bhatshravan <shrvnbhat0@gmail.com>
2020-04-12 15:22:00 +0100

    Update classifier files

--- python/classifier/predict.py
+++ python/classifier/predict.py
@@ -4,10 +4,10 @@ import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv("/content/finals20.csv")
-df = df[["vader","secscore","assoc","perc"]]
+df = df[["vader", "secscore", "assoc", "perc"]]
 print(df.head())
 
-predictors = ['vader','assoc']
+predictors = ['vader', 'assoc']
 target = ['perc']
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_train = scaler.fit_transform(df)
@@ -20,7 +20,7 @@ model.add(tf.keras.layers.Dense(50, activation='relu'))
 model.add(tf.keras.layers.Dense(100, activation='relu'))
 model.add(tf.keras.layers.Dense(50, activation='relu'))
 model.add(tf.keras.layers.Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='mean_squared_error', optimizer='adamax')
 X = scaled_train_df.drop(target, axis=1).values
 Y = scaled_train_df[['perc']].values
 # Train the model
